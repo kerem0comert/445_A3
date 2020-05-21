88d256b15278d2a73c3c806320025ccfde41e07b
Kerem <kerem0comert@gmail.com>
2020-05-21 20:35:36 +0300

    Files created

--- index.py
+++ index.py
@@ -3,8 +3,34 @@
 import os
 import cgi
 
-print("Content-Type: text/html\n")
-print ("Hello Python Web Browser!! This is cool!!")
+def printHeader( title ):
+   print ("""Content-type: text/html
+
+<?xml version = "1.0" encoding = "UTF-8"?
+<!DOCTYPE html PUBLIC
+	"-//W3C//DTD XHTML 1.0 Strict//EN"
+	"DTD/xhtml1-strict.dtd">
+<html xmlns = "http://www.w3.org/1999/xhtml">
+<head><title>%s</title></head>
+<body>""" % title) 
+printHeader("QUERY_STRING example")  
+print ("<h1>Name/Value Pairs</h1>")  
+query = os.environ["QUERY_STRING"]    
+if len(query) == 0:  
+   print ("""<p><br /> 
+      Please add some name-value pairs to the URL above. 
+      Or try 
+      <a href = "index.py?name=Veronica&age=23">this</a>. 
+      </p>""")  
+else:     
+   print ("""<p style = "font-style: italic"> 
+      The query string is '%s'.</p>""" % cgi.escape(query))  
+   pairs = cgi.parse_qs(query)  
+     
+   for key, value in pairs.items():  
+      print ("<p>You set '%s' to value %s</p>" % (key, value))               
+print ("</body></html>")
+
 
 
 
---
+++ register.py
@@ -0,0 +1,4 @@
+#!C:\Program Files\Python36\python.exe
+
+import os
+import cgi
