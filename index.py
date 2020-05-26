60c1e93c6d60fa58c5b4fe35d3e14fc5a9e5e16f
kidobeko@gmail.com <kidobeko@gmail.com>
2020-05-26 17:15:58 +0300

    Move header func to seperate file

--- index.py
+++ index.py
@@ -2,20 +2,9 @@
 
 import os
 import cgi
+from printHeader import printHeader
 
-def printHeader( title ):
-   print ("""Content-type: text/html
-
-<?xml version = "1.0" encoding = "UTF-8"?
-<!DOCTYPE html PUBLIC
-	"-//W3C//DTD XHTML 1.0 Strict//EN"
-	"DTD/xhtml1-strict.dtd">
-<html xmlns = "http://www.w3.org/1999/xhtml">
-<head><title>%s</title></head>
-<body>""" % title) 
-printHeader("Kalkanli Internship System")  
-
-
+printHeader.printHeader("Kalkanli Internship System")  
 form = cgi.FieldStorage()
 
 if 'login' in form and 'username' in form.keys() and 'pwd' in form.keys():
--- login.py
+++ login.py
@@ -1,19 +1,9 @@
 #!C:\Program Files\Python36\python.exe
 import os
 import cgi
+from printHeader import printHeader
 
-
-def printHeader( title ):
-   print ("""Content-type: text/html
-
-<?xml version = "1.0" encoding = "UTF-8"?
-<!DOCTYPE html PUBLIC
-	"-//W3C//DTD XHTML 1.0 Strict//EN"
-	"DTD/xhtml1-strict.dtd">
-<html xmlns = "http://www.w3.org/1999/xhtml">
-<head><title>%s</title></head>
-<body>""" % title) 
-printHeader("KIS Login")  
+printHeader.printHeader("KIS Login")  
 
 
 form = cgi.FieldStorage()
