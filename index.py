8acbacccbb1da0886b078c57c7d14feeb75dcd73
KasliSoykan <58806238+KasliSoykan@users.noreply.github.com>
2020-05-27 22:22:34 +0300

    Quick Fix

--- index.py
+++ index.py
@@ -9,14 +9,17 @@ print("""<p>Welcome to Kalkanli Internship System!</p>""")
 if "HTTP_COOKIE" in os.environ:
 
    print("""<input type="submit" value="Log Out" onclick="window.alert("Not implemented yet!");"/>
-             <input type="submit" value="Create a new Internship" onclick="window.location='createInternship.py';"/> 
-             <input type="submit" value="List Previously Posted Internship Positions " onclick="window.location='listPreviousPositionsCompany.py';"/> """)
+             <input type="submit" value="Create a new Internship" onclick="window.location='createInternship.py';"/> """)
+   print("""<input type="submit" value="List Previously Posted Internship Positions " onclick="window.location='listPreviousPositionsCompany.py';"/>""")
    print("""<input type="submit" value="Search An Internship Position Containing Keyword" onclick="window.location='searchKeywordMain.py';"/>""")
+   print("""<input type="submit" value="List All Active Internship Positions" onclick="window.location='listActivePositions.py';"/>""")
    
 else:
     print("""<input type="submit" value="Log In" onclick="window.location='login.py';"/>""")
     print("""<input type="submit" value="Register" onclick="window.location='register.py';"/>""")
+    print("""<input type="submit" value="Search An Internship Position Containing Keyword" onclick="window.location='searchKeywordMain.py';"/>""")
     print("""<input type="submit" value="List All Active Internship Positions" onclick="window.location='listActivePositions.py';"/>""")
+    
     
 
 htmlMethods.endBodyAndHtml()
