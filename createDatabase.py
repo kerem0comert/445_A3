a4c5b41c19755b0c690259f281df50b0dd49e917
Soykan KAMAL <58806238+KasliSoykan@users.noreply.github.com>
2020-05-30 22:25:05 +0300

    database population

--- createDatabase.py
+++ createDatabase.py
@@ -46,11 +46,79 @@ connection.execute('''INSERT INTO CITY (cityCode,cityName) VALUES (3, 'Guzelyurt
 connection.execute('''INSERT INTO CITY (cityCode,cityName) VALUES (4, 'Iskele');''')
 connection.execute('''INSERT INTO CITY (cityCode,cityName) VALUES (5, 'Lefke');''')
 connection.execute('''INSERT INTO CITY (cityCode,cityName) VALUES (6, 'Lefkosa');''')
-connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('1234','1234', 'google.com', 'Aselsan', 'testmail@gmail.com', '5556667778', 'haha street',1,1);''')
-connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('12345','1234', 'google.com', 'SoykanSan', 'testmail@gmail.com', '5556667778', 'haha street',1,2);''')
-connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (1, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 1234);''')
-connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (2, 'Software Intern', 'very detailed', 'graudate', date('now'), 1234);''')
-connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (3, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 12345);''')
-connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (4, 'Janitor Intern', 'very detailed', 'no expectations', date('now', '+1 days'), 12345);''')
+
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp1','1234', 'google.com', 'Gazi1', 'testmail@gmail.com', '5556667778', 'haha street',1,1);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp2','1234', 'google.com', 'Gazi2', 'testmail@gmail.com', '5556667778', 'haha street',1,1);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp3','1234', 'google.com', 'GirneBeton', 'testmail@gmail.com', '5556667778', 'haha street',1,2);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp4','1234', 'google.com', 'GirneGaz', 'testmail@gmail.com', '5556667778', 'haha street',1,2);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp5','1234', 'google.com', 'Guzelyurt1', 'testmail@gmail.com', '5556667778', 'haha street',1,3);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp6','1234', 'google.com', 'Guzelyurt2', 'testmail@gmail.com', '5556667778', 'haha street',1,3);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp7','1234', 'google.com', 'Iskele1', 'testmail@gmail.com', '5556667778', 'haha street',1,4);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp8','1234', 'google.com', 'Iskele2', 'testmail@gmail.com', '5556667778', 'haha street',1,4);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp9','1234', 'google.com', 'LefkeGaz', 'testmail@gmail.com', '5556667778', 'haha street',1,5);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp10','1234', 'google.com', 'LefkeBoru', 'testmail@gmail.com', '5556667778', 'haha street',1,5);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp11','1234', 'google.com', 'Lefkosa1', 'testmail@gmail.com', '5556667778', 'haha street',1,6);''')
+connection.execute('''INSERT INTO SOFTWARECOMPANY (username,password,website,name,email,telephone,address,sessionId,cityNo) VALUES ('comp12','1234', 'google.com', 'Lefkosa2', 'testmail@gmail.com', '5556667778', 'haha street',1,6);''')
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (1, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp1');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (2, 'Software Intern', 'very detailed', 'graudate', date('now'),  'comp1');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (3, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'),  'comp1');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (4, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp2');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (5, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp2');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (6, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp2');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (7, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp3');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (8, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp3');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (9, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp3');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (10, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp4');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (11, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp4');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (12, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp4');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (13, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp5');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (14, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp5');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (15, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp5');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (16, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp6');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (17, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp6');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (18, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp6');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (19, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp7');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (20, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp7');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (21, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp7');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (22, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp8');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (23, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp8');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (24, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp8');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (25, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp9');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (26, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp9');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (27, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp9');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (28, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp10');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (29, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp10');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (30, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp10');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (31, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp11');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (32, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp11');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (33, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp11');''')
+
+
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (34, 'Testing Engineer', 'nodetail', 'can test stuff', date('now', '-1 days'), 'comp12');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (35, 'Software Intern', 'very detailed', 'graudate', date('now'), 'comp12');''')
+connection.execute('''INSERT INTO INTERNSHIPPOSITION (id,name,details,expectations,deadline,companyUsername) VALUES (36, 'Bell Boy', 'nodetail', 'good loking', date('now', '+1 days'), 'comp12');''')
+
+
 connection.commit()
 connection.close()
